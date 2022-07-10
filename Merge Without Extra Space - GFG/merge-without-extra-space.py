cc27edd9cfd26845268662a4c6ebb8a1d49d7fc4
ketanmishra00 <61942292+ketanmishra00@users.noreply.github.com>
2022-07-11 01:23:14 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.py
+++ Merge Without Extra Space - GFG/merge-without-extra-space.py
@@ -1,30 +1,30 @@
 #User function Template for python3
 
 class Solution:
-    def merge(self, ar1, ar2, n, m): 
-        # code here
+    
+    #Function to merge the arrays.
+    def merge(self,arr1,arr2,n,m):
+        #code here
         arr1.extend(arr2)
         s=arr1.sort()
         arr2.clear()
         return s
   
+    
+
 
 #{ 
 #  Driver Code Starts
-#Initial Template for Python 3
+#Initial template for Python
 
-if __name__ == "__main__": 		
-    tc=int(input())
-    while tc > 0:
-        n, m=map(int, (input().strip().split()))
-        arr1=list(map(int , input().strip().split()))
-        arr2=list(map(int , input().strip().split()))
-        ob = Solution()
-        ans=ob.merge(arr1, arr2, n, m)
-        for x in arr1:
-            print(x, end=" ")
-        for x in arr2:
-            print(x, end=" ")
-        print()
-        tc=tc-1
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        n,m = map(int, input().strip().split())
+        arr1 = list(map(int, input().strip().split()))
+        arr2 = list(map(int, input().strip().split()))
+        ob=Solution()
+        ob.merge(arr1, arr2, n, m)
+        print(*arr1,end=" ")
+        print(*arr2)
 # } Driver Code Ends
