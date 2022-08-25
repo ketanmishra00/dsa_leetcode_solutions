47a8476a0f02eaf3d20031e5567e3fa2d0c31ade
ketanmishra00 <61942292+ketanmishra00@users.noreply.github.com>
2022-08-25 07:46:58 +0530

    Time: 45 ms (97.23%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -4,11 +4,8 @@ class Solution:
         while (t!=0):
             for i in range(t):
                 if ransomNote[i] in magazine:
-                    #index = animals.index('dog')
                     indexs=magazine.index(ransomNote[i])
-                    t-=1
-                    #new_str = test_str[:2] +  test_str[3:]
-                    
+                    t-=1 
                     magazine=magazine[:indexs]+magazine[indexs+1:]
                 elif ransomNote[i] not in magazine:
                     return False
